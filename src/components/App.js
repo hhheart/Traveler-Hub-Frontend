ad8aea66bb58c4cffd886f04c39e6ad5f496bfe3
Thanapol Rojanavichanont <ThanapolR@HMacbookPro.local>
2017-11-01 11:34:30 +0700

    add dataset(json), item_list.json

--- src/components/App.js
+++ src/components/App.js
@@ -1,26 +1,31 @@
-import React, { Component } from 'react';
 
+
+import React, { Component } from 'react';
+//import DataSet from '../static/package_response.json'; 
 //import '././components/css/App.css';
+//var data = require('./package_response.json');
+import ItemList from './item_list';
+
+import MyJson from './data.json'
+
 
 class App extends Component {
-  constructor(props) {
+  /*constructor(props) {
     super(props)
 
-    this.state = {
-      height: 0
-    }
-  }
+  }*/
   
   componentDidMount() {
-    //const height = this.divElement.clientHeight;
-    //this.setState({ height:height });
+    console.log(MyJson);
   }
+
   render() {
     return (
       <div className="App">
         <header className="App-header">
-          <h1 className="App-title">traveler hub</h1>
+            <h1 className="App-title">traveler hub</h1>
         </header>
+        < ItemList />
       </div>
     );
   }
